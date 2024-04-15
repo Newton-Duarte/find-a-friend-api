d510c969c4a7a817a798298948901174b6688214
Newton Duarte <newton_@hotmail.com>
2024-04-15 19:59:55 -0300

    feat: add authenticate controller

---
+++ src/http/controllers/orgs/authenticate.ts
@@ -0,0 +1,65 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
+import { makeAuthenticateService } from '@/services/factories/make-authenticate-service'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const requestBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { email, password } = requestBodySchema.parse(request.body)
+
+  try {
+    const authenticateService = makeAuthenticateService()
+
+    const { organization } = await authenticateService.execute({
+      email,
+      password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: organization.id,
+        },
+      },
+    )
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: organization.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .send({
+        organization: {
+          ...organization,
+          password: undefined,
+        },
+        token,
+      })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+}
