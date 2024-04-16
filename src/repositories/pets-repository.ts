fc58157cd2271a1b0db0c687e3d899fee6d2bda8
Newton Duarte <newton_@hotmail.com>
2024-04-16 11:36:22 -0300

    feat: add find many by city method

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,6 +1,12 @@
 import { Pet, Prisma } from '@prisma/client'
 
+export type FindManyByCityParams = {
+  city: string
+  page: number
+}
+
 export interface PetsRepository {
   findById(id: string): Promise<Pet | null>
+  findManyByCity(params: FindManyByCityParams): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
 }
