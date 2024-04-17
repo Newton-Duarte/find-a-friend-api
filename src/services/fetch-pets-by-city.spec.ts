9be62c83d0d8cdb9423f72364d3b22dc88129a1e
Newton Duarte <newton_@hotmail.com>
2024-04-17 11:05:58 -0300

    test: add pet characteristics unit tests

--- src/services/fetch-pets-by-city.spec.ts
+++ src/services/fetch-pets-by-city.spec.ts
@@ -61,4 +61,147 @@ describe('Fetch Pets By City', () => {
     expect(pets[0].name).toBe('Pet 21')
     expect(pets[1].name).toBe('Pet 22')
   })
+
+  it('should be able to fetch pets by characteristics', async () => {
+    const org = await orgsRepository.create({
+      ...CREATE_ORG_MOCK,
+      city: 'Maceió',
+    })
+
+    const characteristicsPet = await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      age: 'YOUNG',
+      size: 'MEDIUM',
+      energy_level: 'HIGH',
+      independence_level: 'MEDIUM',
+      organization_id: org.id,
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      age: 'ADULT',
+      organization_id: org.id,
+    })
+
+    const { pets } = await sut.execute({
+      city: 'Maceió',
+      page: 1,
+      age: 'YOUNG',
+    })
+
+    expect(pets.length).toBe(1)
+    expect(pets[0]).toEqual(
+      expect.objectContaining({
+        id: characteristicsPet.id,
+      }),
+    )
+  })
+
+  it('should be able to fetch pets by age', async () => {
+    const org = await orgsRepository.create({
+      ...CREATE_ORG_MOCK,
+      city: 'Maceió',
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      age: 'YOUNG',
+      organization_id: org.id,
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      age: 'ADULT',
+      organization_id: org.id,
+    })
+
+    const { pets } = await sut.execute({
+      city: 'Maceió',
+      page: 1,
+      age: 'YOUNG',
+    })
+
+    expect(pets.length).toBe(1)
+  })
+
+  it('should be able to fetch pets by size', async () => {
+    const org = await orgsRepository.create({
+      ...CREATE_ORG_MOCK,
+      city: 'Maceió',
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      size: 'SMALL',
+      organization_id: org.id,
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      size: 'LARGE',
+      organization_id: org.id,
+    })
+
+    const { pets } = await sut.execute({
+      city: 'Maceió',
+      page: 1,
+      size: 'LARGE',
+    })
+
+    expect(pets.length).toBe(1)
+  })
+
+  it('should be able to fetch pets by energy level', async () => {
+    const org = await orgsRepository.create({
+      ...CREATE_ORG_MOCK,
+      city: 'Maceió',
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      energy_level: 'MEDIUM',
+      organization_id: org.id,
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      energy_level: 'HIGH',
+      organization_id: org.id,
+    })
+
+    const { pets } = await sut.execute({
+      city: 'Maceió',
+      page: 1,
+      energyLevel: 'MEDIUM',
+    })
+
+    expect(pets.length).toBe(1)
+  })
+
+  it('should be able to fetch pets by independency level', async () => {
+    const org = await orgsRepository.create({
+      ...CREATE_ORG_MOCK,
+      city: 'Maceió',
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      independence_level: 'LOW',
+      organization_id: org.id,
+    })
+
+    await petsRepository.create({
+      ...CREATE_PET_MOCK,
+      independence_level: 'HIGH',
+      organization_id: org.id,
+    })
+
+    const { pets } = await sut.execute({
+      city: 'Maceió',
+      page: 1,
+      independencyLevel: 'HIGH',
+    })
+
+    expect(pets.length).toBe(1)
+  })
 })
