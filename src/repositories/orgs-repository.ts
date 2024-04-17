e4852148bf11fad40a6920ceb977f0f03fff1af9
Newton Duarte <newton_@hotmail.com>
2024-04-17 11:48:26 -0300

    feat: add find many method to orgs repository interface

--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -3,5 +3,6 @@ import { Organization, Prisma } from '@prisma/client'
 export interface OrgsRepository {
   findById(id: string): Promise<Organization | null>
   findByEmail(email: string): Promise<Organization | null>
+  findMany(): Promise<Organization[]>
   create(data: Prisma.OrganizationCreateInput): Promise<Organization>
 }
