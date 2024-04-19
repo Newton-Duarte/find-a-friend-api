9f94426d3bc0b7a997fbe1c84e0a68c179f5e9c5
Newton Duarte <newton_@hotmail.com>
2024-04-19 20:56:28 -0300

    test: add get pet by id controller e2e tests

---
+++ src/http/controllers/pets/get-by-id.spec.ts
@@ -0,0 +1,44 @@
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { createAndAuthenticateOrg } from '@/utils/test/create-and-authenticate-org'
+
+describe('Get Pet By ID (E2E)', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to get a pet by id', async () => {
+    const { organization, token } = await createAndAuthenticateOrg(app)
+
+    const petResponse = await request(app.server)
+      .post('/pets')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Magali',
+        description: 'Gata Magali',
+        type: 'CAT',
+        size: 'LARGE',
+        age: 'YOUNG',
+        ambient: 'SMALL',
+        independence_level: 'LOW',
+        energy_level: 'LOW',
+        organization_id: organization.id,
+        adoption_requirements: [],
+      })
+
+    const petId = petResponse.body.pet.id
+    const response = await request(app.server).get(`/pets/${petId}`).send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.pet).toEqual(
+      expect.objectContaining({
+        name: 'Magali',
+      }),
+    )
+  })
+})
