38cf396efe550c12711a2ae61f2b464560c622e7
Newton Duarte <newton_@hotmail.com>
2024-04-19 20:55:45 -0300

    feat: add middleware to verify jwt on create pet route

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -2,9 +2,10 @@ import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { search } from './search'
 import { getById } from './get-by-id'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.get('/pets', search)
   app.get('/pets/:id', getById)
-  app.post('/pets', create)
+  app.post('/pets', { onRequest: [verifyJwt] }, create)
 }
